46812deaef1a1fc86e836a61e8b63e5ddefc49de
Ielliena <ielliena122@gmail.com>
2021-11-14 20:21:44 +0500

    Add files via upload

---
+++ 5/code/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> userArray = new List<string>();
+            string userString = "";
+
+            do {
+                userString = Convert.ToString(Console.ReadLine());
+                userArray.Add(userString);
+            } while (userString != "");
+
+            Console.WriteLine("Найти подстроку: ");
+            userString = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Подстрока есть в строках: ");
+            for (int stringIndex = 0; stringIndex < userArray[stringIndex].Length; ++stringIndex) {
+                if(userArray[stringIndex].Contains(userString)) {
+                    Console.WriteLine((stringIndex + 1) + ", ");
+                }
+            }
+        }
+    }
+}
